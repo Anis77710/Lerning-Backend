d843ae5962f908beb21f70aa9c92793fbfe2e56c
Anish77710 <anishguragain77@gmail.com>
2025-05-11 21:50:22 +0545

    2sc

--- unitile.js
+++ unitile.js
@@ -32,10 +32,35 @@ const { CLIENT_RENEG_WINDOW } = require('node:tls');
 //     if(err) console.log(err.message)
 //         else console.log("done");
 // } )
-var http = require('http');
+// var http = require('http');
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+// //create a server object:
+// http.createServer(function (req, res) {
+//   res.write('Hello World!'); //write a response to the client
+//   res.end(); //end the response
+// }).listen(8080); //the server object listens on port 8080
+
+
+
+
+// fs.mkdir("lolu", function(err){
+//   if(err) console.log(err);
+//   else console.log("created a folder");
+  
+  
+// })
+
+
+// fs.readdir("lolu", {withFileTypes: true}, function(err,files){
+//   if(err) console.log(err);
+//   else console.log(files);
+  
+// })
+
+
+fs.rm("lolu", {recursive:true}, function(err){
+  if(err) console.log(err);
+  else console.log("deleted");
+  
+  
+})
